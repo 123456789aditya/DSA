204bec9ca321e18aafff1c7c07917c2f67ff30f4
aditya pathak <pathakaditya881@gmail.com>
2025-03-06 20:56:05 +0530

    updated

--- vector.cpp
+++ vector.cpp
@@ -52,15 +52,122 @@
 
 //Nested Vector...
 
+// #include<iostream>
+// using namespace std;
+// #include<vector>
+// int main(){
+//     int val;
+//     vector<int>v1;
+//     for(int i=0;i<v1.size();i++){
+//         cout<<"enter value"<<endl;
+//         cin>>val;
+        
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// #include<vector>
+// #include<algorithm>
+// bool Cybrom(int a,int b)
+// {
+//     return a>b;
+// }
+// int main(){
+//     vector<int>v{2,1,5,4,6};
+//     sort(v.begin(),v.end());
+//     for(auto p:v)
+//     {
+//         cout<<p<<"\n";
+//     }
+//     cout<<[](int a,int b){return a>b;} (4,20);
+//     cout<<"\n";
+//     sort(v.begin(),v.end(),less<int>());
+//     sort(v.begin(),v.end(),greater<int>());
+//     sort(v.begin(),v.end(),[](int a,int b){return a>b;});
+//     for(auto p:v)
+//     {
+//         cout<<p<<"\t";
+//     }
+
+// }
+
+//inbult function..
+//all_of();none_of(),any_of();..it returns boolean
+
+// auto p=[] (int a,int b) {return a+b;};
+
+//multiply 10 in each element of 2 vectors and add them.
+
+// #include<iostream>
+// using namespace std;
+// #include<vector>
+// #include<algorithm>
+// int main(){
+//     vector<int>v1{1,4,5,8};
+//     vector<int>v2{2,1,1};
+//     int x=0;
+//     int y=0;
+//     for(int i=0;i<v1.size();i++){
+//         x=x*10+v1[i];
+
+//     }
+//     for(int j=0;j<v2.size();j++){
+//         y=y*10+v2[j];
+//     }
+//     cout<<"result="<<x+y;
+// }
+
+
+
+//remove all duplicate values from the vector.
+
+// #include<iostream>
+// using namespace std;
+// #include<vector>
+// #include<algorithm>
+// int main(){
+//     vector<int>v{2,1,2,4,5,4,1,8};
+//     sort(v.begin(),v.end());
+//     int x=1;
+//     for(int a=1;a<v.size();a++){
+//         if(v[x=1]!=v[a]){
+//             v[x]=v[a];
+//             x++;
+//         }
+//     }
+//     for(int i=0;i<x;i++){
+//         cout<<v[i]<<'\t';
+//     }
+// }
+
+//STACK
+//it follows LIFO.last in first out.
+//only four func used PUSH(),POP(),EMPTY(),TOP().
+// we cant show stack on our database.
+
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<stack>
 int main(){
-    int val;
-    vector<int>v1;
-    for(int i=0;i<v1.size();i++){
-        cout<<"enter value"<<endl;
-        cin>>val;
-        
+    stack<int>st;
+    st.push(10);
+    st.push(4);
+    st.push(5);
+    st.push(11);
+    st.push(3);
+    cout<<"result"<<st.empty()<<"\n";
+    cout<<st.top()<<"\n";
+    while(!st.empty()){
+        cout<<st.top()<<"\n";
+        st.pop();
     }
+    cout<<"result="<<st.empty()<<"\n";
+
 }
+
+
+
+
+
