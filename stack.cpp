bf43f83cf304a6116f6f641abb2580c3d0f1a064
aditya pathak <pathakaditya881@gmail.com>
2025-03-12 23:29:47 +0530

    added

--- stack.cpp
+++ stack.cpp
@@ -61,46 +61,46 @@
 // we use only sort() function for arranging data in ascending order
 //reverse() for reversing the f-list data
 
-#include<iostream>
-using namespace std;
-#include<forward_list>
-int main(){
+// #include<iostream>
+// using namespace std;
+// #include<forward_list>
+// int main(){
 
-    forward_list<int>f;
-    f.push_front(3);
-    f.push_front(1);
-    f.push_front(76);
-    f.push_front(8);
-    f.push_front(1);
-    f.sort();
-    f.unique();
-    for(auto p:f){
-        cout<<p<<"\t";
-    }
-    f.pop_front();
-    cout<<"\nafter delete\n";
-    for(auto p:f){
-        cout<<p<<"\t";
-    }
-    cout<<"\n merge \n";
-    forward_list<int>f1{50,3,1,2,5};
-    forward_list<int>f2{4,30,70,20,12};
-    cout<<"\nf1 data\n";
-    for(auto p:f1){
-        cout<<p<<"\t";
-    }
-    cout<<"\n f2 data\n";
-    f1.sort();
-    f2.sort();
-    for(auto p:f2){
-        cout<<p<<"\t";
-    }
-    f1.merge(f2);
-    cout<<"\nafter merging data of f1\n";
-    for(auto p:f1){
+//     forward_list<int>f;
+//     f.push_front(3);
+//     f.push_front(1);
+//     f.push_front(76);
+//     f.push_front(8);
+//     f.push_front(1);
+//     f.sort();
+//     f.unique();
+//     for(auto p:f){
+//         cout<<p<<"\t";
+//     }
+//     f.pop_front();
+//     cout<<"\nafter delete\n";
+//     for(auto p:f){
+//         cout<<p<<"\t";
+//     }
+//     cout<<"\n merge \n";
+//     forward_list<int>f1{50,3,1,2,5};
+//     forward_list<int>f2{4,30,70,20,12};
+//     cout<<"\nf1 data\n";
+//     for(auto p:f1){
+//         cout<<p<<"\t";
+//     }
+//     cout<<"\n f2 data\n";
+//     f1.sort();
+//     f2.sort();
+//     for(auto p:f2){
+//         cout<<p<<"\t";
+//     }
+//     f1.merge(f2);
+//     cout<<"\nafter merging data of f1\n";
+//     for(auto p:f1){
         
-    }
-}
+//     }
+// }
 
 
 
