105e846699558abddb55bf919c13969ffc95cab3
aditya pathak <pathakaditya881@gmail.com>
2025-03-22 23:42:29 +0530

    add

--- Map/map.cpp
+++ Map/map.cpp
@@ -36,37 +36,37 @@
 //unordered map. its timing is much better than sorted map.
 //we cants use complex data type in unordered map because it dosent have its own hashing function.
 
-// #include<iostream>
-// #include<unordered_map>
-// using namespace std;
-// int main(){
-//     unordered_map<int,string>up;
-//     up.insert({10,"anu"});
-//     up.insert({101,"sunny"});
-//     up.insert({73,"ravi"});
-//     for(auto p:up){
-//         cout<<p.first<<" , "<<p.second<<"\n";
-//     }
-// }
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main(){
+    unordered_map<int,string>up;
+    up.insert({10,"anu"});
+    up.insert({101,"sunny"});
+    up.insert({73,"ravi"});
+    for(auto p:up){
+        cout<<p.first<<" , "<<p.second<<"\n";
+    }
+}
 
 ////Multi-map -: it supports duplicate value..
 
-we can delete duplicate value by 2 ways by using iterator and by value. by using iterator we can delete only one value and by using value we can delete multiple values.
-#include<iostream>
-#include<map>
-using namespace std;
-int main(){
-    multimap<int,string>mp;
-    mp.insert({10,'anu'});
-    mp.insert({101,'sunnty'});
-    mp.insert({'10','akj'});
-    mp.insert({14,'upi'});
-    mp.insert({10,'anu'});
-    for(p:mp){
-        cout<<p.first<<":"<<p.second;
+// we can delete duplicate value by 2 ways by using iterator and by value. by using iterator we can delete only one value and by using value we can delete multiple values.
+// #include<iostream>
+// #include<map>
+// using namespace std;
+// int main(){
+//     multimap<int,string>mp;
+//     mp.insert({10,'anu'});
+//     mp.insert({101,'sunnty'});
+//     mp.insert({'10','akj'});
+//     mp.insert({14,'upi'});
+//     mp.insert({10,'anu'});
+//     for(p:mp){
+//         cout<<p.first<<":"<<p.second;
         
-    }
-}
+//     }
+// }
 
 
 
