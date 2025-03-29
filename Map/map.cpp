708aebffd3c59452ab8136049af7bbb1bb0732c8
aditya pathak <pathakaditya881@gmail.com>
2025-03-29 23:40:23 +0530

    add

--- Map/map.cpp
+++ Map/map.cpp
@@ -70,38 +70,38 @@
 
 //self reference class
 
-#include<iostream>
-using namespace std;
-class node{
-    public:int data;
-    node *left;
-    node *right;
-    node(int d){
-        data = d;
-        left=NULL;
-        right=NULL;
-    }
+// #include<iostream>
+// using namespace std;
+// class node{
+//     public:int data;
+//     node *left;
+//     node *right;
+//     node(int d){
+//         data = d;
+//         left=NULL;
+//         right=NULL;
+//     }
 
-};
-int main(){
-    node *root=new node(10);
-    root->left=new node(20);
-    root->right=new node(30);
-    cout<<"preorder"<<endl;
-    cout<<root->data<<"\t";
-    cout<<root->left->data<<"\t";
-    cout<<root->right->data<<"\n";
-    cout<<"Inorder"<<endl;
-    cout<<root->left->data<<"\t";
-    cout<<root->data<<"\t";
-    cout<<root->right->data<<endl;
-    cout<<"postorder"<<endl;
-    cout<<root->left->data<<"\t";
-    cout<<root->right->right<<"\t";
-    cout<<root->data<<endl;
+// };
+// int main(){
+//     node *root=new node(10);
+//     root->left=new node(20);
+//     root->right=new node(30);
+//     cout<<"preorder"<<endl;
+//     cout<<root->data<<"\t";
+//     cout<<root->left->data<<"\t";
+//     cout<<root->right->data<<"\n";
+//     cout<<"Inorder"<<endl;
+//     cout<<root->left->data<<"\t";
+//     cout<<root->data<<"\t";
+//     cout<<root->right->data<<endl;
+//     cout<<"postorder"<<endl;
+//     cout<<root->left->data<<"\t";
+//     cout<<root->right->right<<"\t";
+//     cout<<root->data<<endl;
 
 
-}
+// }
 
 
 
